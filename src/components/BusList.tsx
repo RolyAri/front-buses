976c2b3edf36d9f2b202da9c8bdaa2f08bf4bd99
Roly Ari <109258322+RolyAri@users.noreply.github.com>
2025-04-08 16:55:57 -0500

    añadiendo columnas y formateando fecha

--- src/components/BusList.tsx
+++ src/components/BusList.tsx
@@ -15,6 +15,21 @@ const BusList: React.FC = () => {
     getBuses();
   }, []);
 
+  const formatDate = (date: string) => {
+    let fecha = new Date(date);
+
+    let fechaFormateada = new Intl.DateTimeFormat('es-PE', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    }).format(fecha);
+    return fechaFormateada;
+  }
+
   return (
     <div>
       <Typography variant='h4'>Lista de Buses</Typography>
@@ -25,7 +40,9 @@ const BusList: React.FC = () => {
               <TableCell>ID</TableCell>
               <TableCell>Número de Bus</TableCell>
               <TableCell>Placa</TableCell>
+              <TableCell>Características</TableCell>
               <TableCell>Marca</TableCell>
+              <TableCell>Fecha de Creación</TableCell>
               <TableCell>Estado</TableCell>
             </TableRow>
           </TableHead>
@@ -35,7 +52,9 @@ const BusList: React.FC = () => {
                 <TableCell>{bus.id}</TableCell>
                 <TableCell>{bus.numeroDeBus}</TableCell>
                 <TableCell>{bus.placa}</TableCell>
+                <TableCell>{bus.caracteristicas}</TableCell>
                 <TableCell>{bus.marca.nombre}</TableCell>
+                <TableCell>{formatDate(bus.fechaCreacion)}</TableCell>
                 <TableCell>{bus.activo ? 'Activo' : 'Inactivo'}</TableCell>
               </TableRow>
             ))}
